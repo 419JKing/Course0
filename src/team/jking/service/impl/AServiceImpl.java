1df52779df198933727954ef350549d8dd16b485
cshifeng <wlznlqjyxz@163.com>
2016-10-28 20:38:59 +0800

    22222

--- src/team/jking/service/impl/AServiceImpl.java
+++ src/team/jking/service/impl/AServiceImpl.java
@@ -11,6 +11,7 @@ public class AServiceImpl implements AService {
 	AMapper aMapper;
 	
 	public List<Testtable> getInfos(){
+		System.out.println("2222222");
 		System.out.println("1111");
 		return aMapper.getInfos();
 	}
